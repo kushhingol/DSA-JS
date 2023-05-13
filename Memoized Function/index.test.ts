425ccd4a6f874dd9b8eb0b17f3e0230c0daf2697
kushhingol <kushhingol.kh@gmail.com>
2023-05-13 10:13:29 +0530

    feat(DSA-JS): added solution for memoization in js

---
+++ Memoized Function/index.test.ts
@@ -0,0 +1,31 @@
+import { memoize } from ".";
+
+describe("Test memoize method", () => {
+  test('should return memoized value when fn = sum, inputs = [[2,2],[2,2],[],[1,2],[]] and callStack = ["call","call","getCallCount","call","getCallCount"]', () => {
+    let callCount = 0;
+    const fn = (a, b) => {
+      callCount += 1;
+      return a + b;
+    };
+
+    const inputs = [
+      { params: [2, 2], result: 4 },
+      { params: [2, 2], result: 4 },
+      { params: [], result: 1 },
+      { params: [1, 2], result: 3 },
+      { params: [], result: 2 },
+    ];
+    const callStack = ["call", "call", "getCallCount", "call", "getCallCount"];
+
+    const memoizedMethod = memoize(fn);
+    callStack.forEach((callStackCommand, i) => {
+      if (callStackCommand === "call") {
+        const [a, b] = inputs[i].params;
+        expect(memoizedMethod(a, b)).toBe(inputs[i].result);
+      } else {
+        console.log(callCount);
+        expect(callCount).toBe(inputs[i].result);
+      }
+    });
+  });
+});
