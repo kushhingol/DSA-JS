96ffb1ab743900a4e02b43d579f0bac8135844b4
kushhingol <kushhingol.kh@gmail.com>
2023-05-31 13:12:15 +0530

    added in readme

--- Event Emitter/index.ts
+++ Event Emitter/index.ts
@@ -32,3 +32,15 @@ class EventEmitter {
     return [];
   }
 }
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
