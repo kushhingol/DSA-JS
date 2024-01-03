52bb6224810cce79232e35a0f23580d97a0602c9
kushhingol <kushhingol.kh@gmail.com>
2024-01-03 17:22:01 +0530

    updated method

--- Perfect-Square/index.ts
+++ Perfect-Square/index.ts
@@ -4,18 +4,23 @@ export const isPerfectSquare = (num: number): boolean => {
 };
 
 export const isPerfectSquare2 = (num: number): boolean => {
-  let left = 0;
-  let right = num;
+  let start = 0;
+  let end = num;
 
-  while (left < right) {
-    const mid = left + right / 2;
-    const square = mid ** 2;
-    if (square === num) return true;
-    if (square > num) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+
+    if (middle * middle === num) {
+      return true;
     }
+
+    if (middle * middle > num) {
+      end = middle - 1;
+      continue;
+    }
+
+    start = middle + 1;
   }
+
   return false;
 };
